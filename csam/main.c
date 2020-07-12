b00008b39d7560539b480752a107d21d3276b6ff
liangchengj <liangchengj@outlook.com>
2020-07-13 01:21:27 +0800

    2020/7/13 1:21

---
+++ csam/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    char *usr;
+    char *pwd;
+
+    printf("请输入用户名：");
+    scanf("%s", &usr);
+    printf("请输入密码：");
+    scanf("%s", &pwd);
+
+    return 0;
+}
