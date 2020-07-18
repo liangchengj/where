e63a5134db1de4719f97c93b1c5d25157d70dc27
liangchengj <liangchengj@outlook.com>
2020-07-18 13:35:07 +0800

    Reset.

--- http.dart
+++
@@ -1,8 +0,0 @@
-import 'package:http/http.dart' as http;
-
-var url = 'https://odb.liangchengj.com';
-
-main(List<String> args) async {
-  var resp = await http.get(url);
-  print('Response body: ${resp.body}');
-}
--- json.dart
+++
@@ -1,9 +0,0 @@
-class Boy {
-  var name;
-  var age;
-  Boy({this.name, this.age});
-}
-
-var json = '{"name":"邓捷",age:21}';
-
-main(List<String> args) {}
